60531c09cfc5248c085d88c1b948fca34f0e15b0
Sion Gao <gaoziyuan19@mails.ucas.ac.cn>
2024-10-22 19:36:07 +0800

    add occupy_gpu function for V100

--- occupy_gpu/occupy_gpu.cu
+++ occupy_gpu/occupy_gpu.cu
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <string>
 #include <cuda_runtime.h>
 
-__global__ void dummyKernel(float *a) {
+__global__ void dummyKernel(float *a, int max_iter) {
     int idx = threadIdx.x + blockDim.x * blockIdx.x;
-    a[idx] = sinf(a[idx]);
+    while (true) {
+        a[idx] = sinf(a[idx]);
+    }
 }
 
-void occupyGPU(int device, float gpuRatio, float memRatio) {
+void occupyGPU(int device, float gpuRatio, float memRatio, int max_iter) {
     cudaSetDevice(device);
 
     cudaDeviceProp prop;
@@ -22,21 +25,41 @@ void occupyGPU(int device, float gpuRatio, float memRatio) {
     // 启动 CUDA 核心函数
     const int blockSize = 64;
     const int SMCount = prop.multiProcessorCount;
-    const int numBlocks = 2;
-    // const int numBlocks = 32 * SMCount * gpuRatio;
+    const int total_threads = 2048 * SMCount * gpuRatio;
+    int calculatedBlocks = total_threads / blockSize;
+    if (calculatedBlocks < 4) {
+        calculatedBlocks = 4;
+    }
+    const int numBlocks = (calculatedBlocks - (calculatedBlocks % 4));
+    std::cout << "SM count: " << SMCount << ", numBlocks: " << numBlocks << std::endl;
 
     while (true) {
-        dummyKernel<<<numBlocks, blockSize>>>(d_mem);
+        dummyKernel<<<numBlocks, blockSize>>>(d_mem, max_iter);
         cudaDeviceSynchronize();
     }
 
     cudaFree(d_mem);
 }
 
-int main() {
+int main(int argc, char **argv) {
     float gpuRatio = 0.4, memRatio = 0.4;
-    int deviceIndex = 0;
+    if (argc >= 3) {
+        std::cout << "argc: " << argc << ", argv[1]: " << argv[1] << std::endl;
+        gpuRatio = (float)std::stoi(argv[1]) / 100.0;
+        memRatio = (float)std::stoi(argv[2]) / 100.0;
+    }
+    std::cout << "GPU ratio: " << gpuRatio << ", memory ratio: " << memRatio << std::endl;
 
-    occupyGPU(deviceIndex, gpuRatio, memRatio);
+    int deviceIndex = 0;
+    if (argc >= 4) {
+        deviceIndex = std::stoi(argv[3]);
+    }
+
+    int max_iter = 10;
+    if (argc >= 5) {
+        max_iter = std::stoi(argv[4]);
+    }
+
+    occupyGPU(deviceIndex, gpuRatio, memRatio, max_iter);
     return 0;
 }
