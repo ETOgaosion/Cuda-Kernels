4b84bbaa2f214db94f9ff2e2c9db7a0d488c0510
Sion Gao <gaoziyuan19@mails.ucas.ac.cn>
2024-10-22 19:36:57 +0800

    add occupy_gpu function for V100

--- occupy_gpu/check_sm.sh
+++ occupy_gpu/check_sm.sh
@@ -2,10 +2,8 @@
 
 pid=$(ps aux | grep '\./occupy_gpu' | grep -v grep | awk '{print $2}' | head -n 1)
 
-# 检查是否有找到进程
 if [ -z "$pid" ]; then
     echo "not found 'project_pactum' process."
 else
-    # 循环终止每个PID对应的进程
     dcgmi dmon -e 1002,1003,1004,1006,1007,1008,1005,1009,1001,1011,1012 > res.txt
 fi
