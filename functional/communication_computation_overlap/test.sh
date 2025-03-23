055a2df75d4e4123df6fcd1b145b4e85af9868bc
BlueSpace <gaoziyuan19@mails.ucas.ac.cn>
2025-03-23 18:38:07 +0000

    fix test.sh by add master_addr and master_port cmd line args

--- functional/communication_computation_overlap/test.sh
+++ functional/communication_computation_overlap/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export MASTER_ADDR=localhost
-export MASTER_PORT=7000
+MASTER_ADDR=localhost
+MASTER_PORT=7000
 
-torchrun --nnodes=1 --nproc_per_node=2 test.py
+torchrun --nnodes=1 --nproc_per_node=2 --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT test.py
